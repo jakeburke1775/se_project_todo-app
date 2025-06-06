83bc7f326ea7c2df81a9c027116fe7950d5bfcfe
Jake B <jakeburke1775@gmail.com>
2025-06-06 08:28:44 -0400

    change task items

--- utils/constants.js
+++ utils/constants.js
@@ -1,25 +1,25 @@
 const initialTodos = [
   {
     id: "aa486839-63ab-437f-b8a2-29ab217dff4f",
-    name: "pet / eco friendly availible?   Mention probiotic cleaning",
+    name: "Take out trash",
     completed: false,
     date: new Date(),
   },
   {
     id: "aa486839-63ab-437f-b8a2-29ab217dff4f",
-    name: "Discuss where and the times they clean",
+    name: "Do the dishes",
     completed: false,
     date: new Date(),
   },
   {
     id: "a7bfd5ef-37cc-4fa6-89f2-cac098a8aeba",
-    name: "INTRO ITEM: Discuss pay per hour (3 hour example once a week)",
+    name: "Give dog a bath",
     completed: false,
     date: new Date(),
   },
   {
     id: "7cec7373-681b-49d9-b065-021d61a69d03",
-    name: "INTRO ITEM: Discuss Cleaner availibility",
+    name: "make kids lunch for tomorrow",
     completed: false,
     date: new Date(),
   },
